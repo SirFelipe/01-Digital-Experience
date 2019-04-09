11b383de1658e87127ee12df7f948ee17cf67acd
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-08 22:48:59 -0300

    Creating my CMS - separating html with include files

---
+++ cms/includes/navigation.php
@@ -0,0 +1,44 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <?php
+                $query = "SELECT * FROM T_CATEGORIA";
+                $select_categorias = mysqli_query($connection, $query);
+
+                //TRAZENDO O NOME DAS CATEGORIAS DO DB COM ARRAY ASSOCIATIVO.
+                while($row = mysqli_fetch_assoc($select_categorias)){
+                  $nm_categoria = $row['nm_cat'];
+                  /* foreach ($row as $key) { echo $key;}*/
+                  echo '<li><a href="#">' . $nm_categoria . '</a></li>';
+                }
+               ?>
+              <
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
+                <!--
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li> -->
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
---
+++ cms/index.php
@@ -0,0 +1,58 @@
+<!-- DATABASE -->
+<?php include "includes/db.php"; ?>
+<!-- HEADER -->
+<?php include "includes/header.php";?>
+<!-- NAVIGATION -->
+<?php include "includes/navigation.php";?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <?php
+                  $query = "SELECT * FROM T_POST";
+                  $select_posts = mysqli_query($connection, $query);
+
+                  while($row = mysqli_fetch_assoc($select_posts)){
+                    $nm_titulo = $row['nm_titulo'];
+                    $nm_autor = $row['nm_autor'];
+                    $dt_post = $row['dt_post'];
+                    $ds_conteudo = $row['ds_conteudo'];
+                    $img_post = $row['img_post'];
+                    $ds_tags = $row['ds_tags'];
+                  }
+                ?>
+
+                <h2>
+                  <a href="#"><?php echo $nm_titulo; ?></a>
+                </h2>
+                <p class="lead">
+                  por
+                  <a href="index.php"><?php echo $nm_autor; ?></a></p>
+                  <p><span class="glyphicon glyphicon-time"></span> Postado em <?php $dt_post = date('d-m-Y', strtotime($dt_post)); ?></p>
+                  <hr>
+                  <img class="img-responsive" src="img/<?php echo $img_post; ?>" alt=""> <hr>
+                  <p><?php echo $ds_conteudo; ?></p>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/sidebar.php";?>
+
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- FOOTER -->
+        <?php include "includes/footer.php";?>
